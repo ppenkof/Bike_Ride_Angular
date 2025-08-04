6ab6c6fb8eb9a07640eaf54dd04ad6c6eefd65d4
ppenkof <overton@abv.bg>
2025-08-04 11:40:33 +0300

    create feature components

---
+++ Bike_Ride_Angular/Bike_Ride/BikeRideApp/Client/Bike_Ride/src/app/features/bike-board/bike-board.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bike-board',
+  imports: [],
+  templateUrl: './bike-board.html',
+  styleUrl: './bike-board.css'
+})
+export class BikeBoard {
+
+}
---
+++ Bike_Ride_Angular/Bike_Ride/BikeRideApp/Client/Bike_Ride/src/app/features/bike-item/bike-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bike-item',
+  imports: [],
+  templateUrl: './bike-item.html',
+  styleUrl: './bike-item.css'
+})
+export class BikeItem {
+
+}
