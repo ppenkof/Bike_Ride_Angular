0048c803d2e584a96d66cd5e428b044d4644dfd6
ppenkof <overton@abv.bg>
2025-08-12 15:25:30 +0300

    Guard, Service and Interceptors created

--- Bike_Ride_Angular/Bike_Ride/BikeRideApp/Client/Bike_Ride/src/app/features/bikesCs/bike-board/bike-board.ts
+++ Bike_Ride_Angular/Bike_Ride/BikeRideApp/Client/Bike_Ride/src/app/features/bikesCs/bike-board/bike-board.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bike-board',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './bike-board.html',
   styleUrl: './bike-board.css'
 })
-export class BikeBoard {
+export class BikeBoard {}
 
-}
