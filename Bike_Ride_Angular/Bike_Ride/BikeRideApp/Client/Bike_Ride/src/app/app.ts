de4bdf36fe117756476acc906ec6e07b4412a69d
ppenkof <overton@abv.bg>
2025-08-13 23:51:14 +0300

    fix syntax issue

--- Bike_Ride_Angular/Bike_Ride/BikeRideApp/Client/Bike_Ride/src/app/app.ts
+++ Bike_Ride_Angular/Bike_Ride/BikeRideApp/Client/Bike_Ride/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer, Header, ErrorNotification} from './shared/components';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+            Footer, 
+            Header,
+            ErrorNotification],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
